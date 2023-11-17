f6ebbba9b06db6c9c6b96e8621d0047b7d6f061e
sinda <sinda.ben_yahmed@etud.univ-jfc.fr>
2023-11-17 10:23:46 +0100

    commit personne

--- src/fr/valentinisis/asterix/classes/personne/Mechant.java
+++ src/fr/valentinisis/asterix/classes/personne/Mechant.java
@@ -1,4 +1,14 @@
 package fr.valentinisis.asterix.classes.personne;
 
-public class Mechant {
+public class Mechant extends Personne  {
+private int ForceCombat;
+public Mechant(String nom ,int force ,int ForceCombat){
+    super(nom,force);
+    this.ForceCombat=ForceCombat;
+}
+    public int getForceCombat(){
+    return ForceCombat;
+    }
+//public void recupererForce() {
+      //  {}
 }
