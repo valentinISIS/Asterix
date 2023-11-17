944672f4ee901e7d3e484743fa53a6cd524d1038
Valentin ISIS <valentin.beaumelle@etud.univ-jfc.fr>
2023-11-17 11:40:04 +0100

    correction de la classe Druide

--- src/fr/valentinisis/asterix/classes/personne/Druide.java
+++ src/fr/valentinisis/asterix/classes/personne/Druide.java
@@ -4,33 +4,22 @@ public class Druide extends Gaulois {
 
     private static final int VALEUR_MIN = 1;
     private static final int VALEUR_MAX =20 ;
-    private int potion;
+    private int potion = 0;
 
 
-    public Druide(String nom, int force,String metier ,int potion) {
-        super(nom,force,metier);
-        this.potion = potion;
+    public Druide(String nom, int force) {
+        super(nom,force,"druide");
     }
 
 
     public void fabriquerPotion() {
         // Logique pour fabriquer la potion
-        Random random = new Random();
-        int nouvellePotion = VALEUR_MIN + random.nextInt(VALEUR_MAX - VALEUR_MIN + 1);
-        this.potion = nouvellePotion;
+        this.potion = new Random().nextInt(VALEUR_MIN, VALEUR_MAX+1);
 
         System.out.println("Le Druide a fabriqué une nouvelle potion. Nouvelle valeur de la potion : " + potion);
     }
 
-
-    public void utiliserPotion(int nouvellePotion) {
-        // Logique pour utiliser la potion
-        if (nouvellePotion >= VALEUR_MIN && nouvellePotion <= VALEUR_MAX) {
-            this.potion = nouvellePotion;
-            System.out.println("Le Druide a utilisé une nouvelle potion. Nouvelle valeur de la potion : " + potion);
-        } else {
-            System.out.println("La valeur de la potion doit être comprise entre " + VALEUR_MIN + " et " + VALEUR_MAX);
-        }
+    public int getPotion() {
+        return potion;
     }
-
 }
