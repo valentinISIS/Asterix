90b1242d63f22bfe80cd3197ad342a1a9a5d338c
sinda <sinda.ben_yahmed@etud.univ-jfc.fr>
2023-11-17 10:40:05 +0100

    commit Druide

--- src/fr/valentinisis/asterix/classes/personne/Druide.java
+++ src/fr/valentinisis/asterix/classes/personne/Druide.java
@@ -1,4 +1,24 @@
 package fr.valentinisis.asterix.classes.personne;
 
-public class Druide {
+public class Druide extends Gaulois {
+
+    private static final int VALEUR_MIN = 1;
+    private static final int VALEUR_MAX =20 ;
+    private int potion;
+
+
+    public Druide(String nom, int force,String metier ,int potion) {
+        super(nom,force,metier);
+        this.potion = potion;
+    }
+
+
+
+    public void boirePotion(int montant) {
+        this.potion += montant;
+        this.setForce(this.getForce() + montant);
+        System.out.println( getNom() + " boit une potion. Nouvelle force : " + getForce());
+    }
+
+
 }
