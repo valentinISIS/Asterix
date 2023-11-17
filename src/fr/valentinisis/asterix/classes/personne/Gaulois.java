5a831073f4d02f7a5a6e168c6174efa3ed454738
sinda <sinda.ben_yahmed@etud.univ-jfc.fr>
2023-11-17 10:48:03 +0100

    commit Gaulois

--- src/fr/valentinisis/asterix/classes/personne/Gaulois.java
+++ src/fr/valentinisis/asterix/classes/personne/Gaulois.java
@@ -1,4 +1,28 @@
 package fr.valentinisis.asterix.classes.personne;
 
-public class Gaulois {
-}
+public class Gaulois extends Personne{
+        private String metier;
+
+
+        public Gaulois(String nom, int force, String metier) {
+            super(nom, force);
+            this.metier = metier;
+
+        }
+
+        public String getMetier() {
+            return metier;
+        }
+
+        public void setMetier(String metier) {
+            this.metier = metier;
+        }
+
+    public void boirePotion(int montant) {
+        // Correction ici
+        setForce(getForce() + montant);
+        System.out.println(getNom() + " boit une potion. Nouvelle force : " + getForce());
+    }
+    }
+
+
