680b97a3fd50356677266287eaad3d92e61b5254
sinda <sinda.ben_yahmed@etud.univ-jfc.fr>
2023-11-17 10:25:22 +0100

    commit personne

--- src/fr/valentinisis/asterix/classes/personne/Romain.java
+++ src/fr/valentinisis/asterix/classes/personne/Romain.java
@@ -1,4 +1,12 @@
 package fr.valentinisis.asterix.classes.personne;
 
-public class Romain {
+public class Romain extends Mechant{
+    private Grade grade;
+    public Romain (String nom, int force , int ForceCombat ,Grade grade){
+        super(nom,force, ForceCombat);
+        this.grade=grade;
+    }
+public Grade getGrade()
+{return grade;}
+
 }
