2f043deced70a35218648449de354d2b3ee03c79
sinda <sinda.ben_yahmed@etud.univ-jfc.fr>
2023-11-17 10:00:30 +0100

    commit personne

--- src/fr/valentinisis/asterix/classes/personne/Personne.java
+++ src/fr/valentinisis/asterix/classes/personne/Personne.java
@@ -1,4 +1,29 @@
 package fr.valentinisis.asterix.classes.personne;
 
 public class Personne {
+    private String nom;
+    private int force;
+
+
+    public Personne(String nom, int force) {
+        this.nom = nom;
+        this.force = force;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public int getForce() {
+        return force;
+    }
+
+    public void setForce(int force) {
+        this.force = force;
+    }
 }
